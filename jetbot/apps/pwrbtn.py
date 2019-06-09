c2d1b2ba606eb1e92638cdd5395981a66aaebc68
Frank Zhao <frank.zhao.main@gmail.com>
2019-06-09 12:23:39 -0700

    rewrote pwrbtn.py to avoid some threading issues

--- jetbot/apps/pwrbtn.py
+++ jetbot/apps/pwrbtn.py
@@ -6,54 +6,52 @@ import subprocess
 import RPi.GPIO as GPIO
 
 but_pin = 13
-prev_btn_val = GPIO.HIGH
-time_rise = False
-time_fall = False
-prev_pulse = False
 flag_pressed = False
 
-def btn_event(channel):
-	global but_pin
-	global prev_btn_val
-	global time_rise
-	global time_fall
-	global flag_pressed
-	print("event\n")
-	v = GPIO.input(but_pin)
-	t = datetime.datetime.now()
-	if v == GPIO.HIGH and prev_btn_val == GPIO.LOW:
-		time_rise = t
-		if time_fall != False:
-			prev_pulse = time_rise - time_fall
-		time_fall = False
-	elif v == GPIO.LOW and prev_btn_val == GPIO.HIGH:
-		time_fall = t
-		flag_pressed = True
-	prev_btn_val = v
+# def btn_event(channel):
+	# global but_pin
+	# global flag_pressed
+	# print("event")
+	# v = GPIO.input(but_pin)
+	# t = datetime.datetime.now()
+	# if v == GPIO.LOW:
+		# flag_pressed = True
 
 if __name__ == '__main__':
 	try:
+		print("setup")
 		GPIO.setwarnings(False)
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(but_pin, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
-		GPIO.add_event_detect(but_pin, GPIO.BOTH, callback=btn_event, bouncetime=10)
-		print("setup\n")
-		while True:
-			now = datetime.datetime.now()
-			if flag_pressed:
-				flag_pressed = False
-				subprocess.run("sync") # flush cache to disk
-				print("sync\n")
-				# todo: issue lock command to motor driver
-			if time_fall != False:
-				timediff = now - time_fall
-				if timediff.total_seconds() > 3:
-					print("shutdown\n")
-					subprocess.run("shutdown now", shell=True)
-					while True:
-						time.sleep(1)
-						# do nothing, wait for shutdown
+		while GPIO.input(but_pin) == GPIO.LOW:
+			print("waiting for initial released state")
 			time.sleep(1)
+		GPIO.add_event_detect(but_pin, GPIO.BOTH, bouncetime=10)
+		while True:
+			if GPIO.event_detected(but_pin):
+				#flag_pressed = False
+				#subprocess.run("sync") # flush cache to disk
+				print("event")
+				# todo: issue lock command to motor driver
+			elif GPIO.input(but_pin) == GPIO.LOW:
+				heldcnt = 0
+				heldlimit = 400
+				while GPIO.input(but_pin) == GPIO.LOW and heldcnt < heldlimit:
+					heldcnt += 1
+					time.sleep(0.01)
+				if heldcnt >= heldlimit:
+					print("shutdown")
+					#subprocess.run("shutdown now", shell=True)
+					quit()
+					#while True:
+					#	time.sleep(1)
+					#	# do nothing, wait for shutdown
+			else:
+				time.sleep(1)
 
 	finally:
-		GPIO.cleanup()
+		try:
+			print("cleanup")
+			GPIO.cleanup()
+		except:
+			print("exception while cleaning up")
