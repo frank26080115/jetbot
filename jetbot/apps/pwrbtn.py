12efafef55794784b0ebe3684b42d08ae389805c
Frank Zhao <frank.zhao.main@gmail.com>
2019-06-10 00:32:41 -0700

    changed to active-high button for pwrbtn

--- jetbot/apps/pwrbtn.py
+++ jetbot/apps/pwrbtn.py
@@ -6,46 +6,39 @@ import subprocess
 import RPi.GPIO as GPIO
 
 but_pin = 13
-flag_pressed = False
-
-# def btn_event(channel):
-	# global but_pin
-	# global flag_pressed
-	# print("event")
-	# v = GPIO.input(but_pin)
-	# t = datetime.datetime.now()
-	# if v == GPIO.LOW:
-		# flag_pressed = True
 
 if __name__ == '__main__':
 	try:
 		print("setup")
 		GPIO.setwarnings(False)
 		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(but_pin, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
-		while GPIO.input(but_pin) == GPIO.LOW:
+		GPIO.setup(but_pin, GPIO.IN)
+		time.sleep(0.1)
+		waspressed = False
+		while GPIO.input(but_pin) == GPIO.HIGH:
+			GPIO.setup(but_pin, GPIO.IN)
+			waspressed = True
 			print("waiting for initial released state")
 			time.sleep(1)
-		GPIO.add_event_detect(but_pin, GPIO.BOTH, bouncetime=10)
+		if waspressed:
+			print("release has occured")
 		while True:
-			if GPIO.event_detected(but_pin):
-				#flag_pressed = False
-				#subprocess.run("sync") # flush cache to disk
-				print("event")
-				# todo: issue lock command to motor driver
-			elif GPIO.input(but_pin) == GPIO.LOW:
+			if GPIO.input(but_pin) == GPIO.HIGH:
 				heldcnt = 0
 				heldlimit = 400
-				while GPIO.input(but_pin) == GPIO.LOW and heldcnt < heldlimit:
+				print("press")
+				subprocess.run("sync") # flush cache to disk
+				while GPIO.input(but_pin) == GPIO.HIGH and heldcnt < heldlimit:
 					heldcnt += 1
 					time.sleep(0.01)
 				if heldcnt >= heldlimit:
 					print("shutdown")
 					#subprocess.run("shutdown now", shell=True)
 					quit()
-					#while True:
+					#while True: # do nothing, wait for shutdown
 					#	time.sleep(1)
-					#	# do nothing, wait for shutdown
+				else:
+					print("short press")
 			else:
 				time.sleep(1)
 
