96aed2c49b2a4876e2454948124fb6fdfb3c8429
Frank Zhao <frank.zhao.main@gmail.com>
2019-06-16 22:03:11 -0700

    implemented undistort in camera class

--- jetbot/camera.py
+++ jetbot/camera.py
@@ -4,7 +4,7 @@ import atexit
 from cv2 import VideoCapture, CAP_GSTREAMER
 import threading
 import numpy as np
-
+from jetbot.utils.undistort import undistort_image
 
 class Camera(SingletonConfigurable):
     
@@ -21,6 +21,14 @@ class Camera(SingletonConfigurable):
     def __init__(self, *args, **kwargs):
         self.value = np.empty((self.height, self.width, 3), dtype=np.uint8)
         super(Camera, self).__init__(*args, **kwargs)
+
+        self.undistort = False
+        self.undistort_dim = None
+        self.undistort_k = None
+        self.undistort_d = None
+        self.undistort_balance = 0
+        self.undistort_dim2 = None
+        self.undistort_dim3 = None
 
         try:
             self.cap = VideoCapture(self._gst_str(), CAP_GSTREAMER)
@@ -43,7 +51,7 @@ class Camera(SingletonConfigurable):
         while True:
             re, image = self.cap.read()
             if re:
-                self.value = image
+                self.value = self.post_process_image(image)
             else:
                 break
                 
@@ -67,3 +75,35 @@ class Camera(SingletonConfigurable):
     def restart(self):
         self.stop()
         self.start()
+
+    def enable_undistort(self, balance=0.0, dim2=None, dim3=None):
+        self.undistort_balance = balance
+        self.undistort_dim2 = dim2
+        self.undistort_dim3 = dim3
+        if self.width == self.height:
+            self.undistort_dim = (self.width, self.height)
+            self.undistort_k = np.array([[346.7104965474094, 0.0, 379.81851823551904], [0.0, 462.1778768584941, 355.5060529230117], [0.0, 0.0, 1.0]])
+            self.undistort_d = np.array([[-0.04498282438799088], [0.07821348164549044], [-0.15085637279264702], [0.092845374923209]])
+            self.undistort = True
+        else:
+            asp_self = self.height / self.width
+            asp_shouldbe = 3 / 4
+            if round(asp_self * 100) == round(asp_shouldbe * 100):
+                self.undistort_dim = (self.width, self.height)
+                self.undistort_k = np.array([[460.70753878093376, 0.0, 505.24222903420747], [0.0, 461.22079408566924, 357.7314598652513], [0.0, 0.0, 1.0]])
+                self.undistort_d = np.array([[-0.016987487065241266], [-0.014653716767538133], [-0.02029760249000433], [0.016954509783374646]])
+                self.undistort = True
+            else:
+                # allow the caller to load up the required parameters manually
+                if self.undistort_dim != None and self.undistort_k != None and self.undistort_d != None:
+                    self.undistort = True
+                else:
+                    self.undistort = False
+
+    def disable_undistort(self):
+        self.undistort = False
+
+    def post_process_image(self, img):
+        if self.undistort:
+            img = undistort_image(img, self.undistort_dim, self.undistort_k, self.undistort_d, balance=self.undistort_balance, dim2=self.undistort_dim2, dim3=self.undistort_dim3)
+        return img
