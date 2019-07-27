1397a620921ebc5c1895df842e3906feae52a248
Frank Zhao <frank.zhao.main@gmail.com>
2019-07-26 22:33:36 -0700

    added min and max to perftimer

--- sloth/perftimer.py
+++ sloth/perftimer.py
@@ -1,4 +1,4 @@
-import time
+import time, sys
 
 FILTER_CONST = 0.1
 
@@ -9,6 +9,8 @@ class PerfTimer(object):
 		self.lt = None
 		self.fps = -1
 		self.fps_filtered = -1
+		self.max = 0
+		self.min = sys.float_info.max
 
 	def tick(self):
 		self.t = time.perf_counter()
@@ -21,9 +23,21 @@ class PerfTimer(object):
 			self.fps_filtered = self.fps
 		self.fps_filtered = (self.fps_filtered * (1.0 - FILTER_CONST)) + (self.fps * FILTER_CONST)
 		self.lt = self.t
+		if self.fps > self.max:
+			self.max = self.fps
+		if self.fps < self.min:
+			self.min = self.fps
 
 	def get_framerate(self):
 		return self.fps_filtered
+
+	def get_stats(self):
+		return self.min, self.max
+
+	def reset_stats(self):
+		self.max = 0
+		self.min = sys.float_info.max
+
 
 mine = PerfTimer()
 
@@ -34,3 +48,11 @@ def tick():
 def get_framerate():
 	global mine
 	return mine.get_framerate()
+
+def get_stats(self):
+	global mine
+	return mine.get_stats()
+
+def reset_stats(self):
+	global mine
+	mine.reset_stats()
